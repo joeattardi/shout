08f6f94952724408043db9717ceeca84de18383b
Joe Attardi <joe@attardi.net>
2018-07-26 17:26:28 -0400

    move getUniqueSlug to util

--- server/handlers/admin/rooms/create-room.js
+++ server/handlers/admin/rooms/create-room.js
@@ -1,13 +1,12 @@
 const { check } = require('express-validator/check');
 const { Op } = require('sequelize');
 const { pick } = require('lodash');
-const slug = require('slug');
 
 const Room = require('../../../../models').Room;
 const logger = require('../../../logger');
 
 const { Result, sendResult } = require('../../../api');
-const { getRoomForSlug } = require('./util');
+const { getUniqueSlug } = require('./util');
 
 exports.handler = async function(req, res) {
   logger.info(`Creating new room "${req.body.name}"`);
@@ -41,22 +40,6 @@ exports.handler = async function(req, res) {
     sendResult(res, 500, Result.ERROR, 'An unexpected error has occurred');
   }
 };
-
-async function getUniqueSlug(name) {
-  let uniqueSlug = slug(name);
-  logger.debug(`Got slug: ${uniqueSlug}`);
-  let room = await getRoomForSlug(uniqueSlug);
-
-  let suffix = 1;
-  while (room) {
-    uniqueSlug = `${slug(name)}-${suffix++}`;
-    logger.debug(`Slug already in use, trying ${uniqueSlug}`);
-    room = await getRoomForSlug(uniqueSlug);
-  }
-
-  logger.debug(`Found unique slug: ${uniqueSlug}`);
-  return uniqueSlug.toLowerCase();
-}
 
 exports.validation = [
   check('name', 'Name is required')
--- server/handlers/admin/rooms/util.js
+++ server/handlers/admin/rooms/util.js
@@ -1,5 +1,7 @@
 const { Op } = require('sequelize');
+const slug = require('slug');
 
+const logger = require('../../../logger');
 const Room = require('../../../../models').Room;
 
 exports.getRoomForSlug = function getRoomForSlug(roomSlug) {
@@ -10,4 +12,30 @@ exports.getRoomForSlug = function getRoomForSlug(roomSlug) {
       }
     }
   });
-}
+};
+
+exports.getRoomForName = function getRoomForName(roomName) {
+  return Room.findOne({
+    where: {
+      name: {
+        [Op.iLike]: roomName
+      }
+    }
+  });
+};
+
+exports.getUniqueSlug = async function getUniqueSlug(name) {
+  let uniqueSlug = slug(name);
+  logger.debug(`Got slug: ${uniqueSlug}`);
+  let room = await exports.getRoomForSlug(uniqueSlug);
+
+  let suffix = 1;
+  while (room) {
+    uniqueSlug = `${slug(name)}-${suffix++}`;
+    logger.debug(`Slug already in use, trying ${uniqueSlug}`);
+    room = await exports.getRoomForSlug(uniqueSlug);
+  }
+
+  logger.debug(`Found unique slug: ${uniqueSlug}`);
+  return uniqueSlug.toLowerCase();
+};
